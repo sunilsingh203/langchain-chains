3c786d145f4474afa75b8bad9c175dea459d860d
Sunil Singh <sunilsingh81188@gmail.com>
2025-11-11 22:43:04 +0530

    Refactor parallel_chain.py for clarity and structure
    from "langchain.schema.runnable" import RunnableParallel
    is no longer
    They moved all “runnable” utilities into "langchain_core.runnables."

--- parallel_chain.py
+++ parallel_chain.py
@@ -3,7 +3,8 @@ from langchain_anthropic import ChatAnthropic
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain.schema.runnable import RunnableParallel
+from langchain_core.runnables import RunnableParallel
+
 
 load_dotenv()
 
@@ -64,4 +65,5 @@ result = chain.invoke({'text':text})
 print(result)
 
 chain.get_graph().print_ascii()
+
 
